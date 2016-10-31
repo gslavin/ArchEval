2bff4b497941460c91c234ae940d9a09faee65f6
grs52 <grs52@ECE-BSN615-L01.coe.drexel.edu>
2016-10-31 13:43:24 -0400

    Modified the DSE_search based on our previous talks

--- DSE_search/DSE_searcher.py
+++ DSE_search/DSE_searcher.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 #
 # ECE49X - Senior Design
 # Eric Rock, George Slavin, Avik Bag
@@ -7,39 +9,90 @@
 # Stochastic Hill Climbing Algorithm
 #
 
-import string
-import sys
-
-import random
+import itertools as it
+import random as r
 
 
 class DSE_searcher:
+    """
+    Wrapper class for the design space exploration search.
+    Uses the simulation wrappers to a run a variety of simulations
+    """
 
-    def __init__( self, search_params, max_num_generations = 10, search_parties = 2 ):
-        self.search_params = search_params
-        self.search_state = []
+    def __init__(self, user_constraints, max_iterations = 10, num_search_parties = 2):
+
+        self.max_iterations = max_iterations
+        self.num_search_parties = num_search_parties
+        self.user_constraints = user_constraints
+        self.sys_configs = []
+
+        # Generate the intial config for each search party
+        self.sys_configs = list(it.islice(self.gen_inital_sys_config(),
+                                                  self.num_search_parties))
+
+        print(self.sys_configs)
+
+    def gen_inital_sys_config(self):
+        """
+        Generates a stream of non-repeating system configuration
+        based on the user constraints
+        """
+        prev_configs = []
+        while True:
+            config = self.gen_rand_config()
+            while config in prev_configs:
+               config = self.gen_rand_config()
+            prev_configs.append(config)
+            yield config
+
+    # TODO: Determine how to establish initial target values to optimize
+    def gen_rand_config(self):
+        """
+        Use the user constraints to generate a random configuration
+        """
+        config = {}
+        config["cpu_count"] = r.randint(1, 8)
+        config["cpu_frequency"] = r.randint(1,7)*1000000000
+        config["cache_size"] =  2**(r.randint(15,25))
+        return config
 
 
-        self.max_num_generations = max_num_generations
-        self.search_parties = search_parties
+    def search(self):
+        """
+        Top level search
+        Each search party starts at its random starting point
+        Neighbors around the starting point are tested
+        The best neighbor is chosen
+        """
+        for _ in range(self.max_iterations):
+            for i in range(self.num_search_parties):
+                # Each party will start a hill climbing search during each iteration
+                new_sys_config = self.search_neighbors(self.sys_configs[i])
+                if (new_sys_config == self.sys_configs[i]):
+                    # Current nodes is a local max or min
+                    pass
+                self.sys_configs[i] = new_sys_config
 
-        for i in range(self.search_parties):
-            self.search_state.append([])
+    def search_neighbors(self, sys_config):
+        """
+        Searches neighbor nodes to see if thye provide a better score
+        Uses simulation wrappers
+        """
+        # Generate possible neighbors
+        neighbors = []
+        for n in neighbors:
+            # Run simulations (via simulation wrappers)
+            # score simulation results according to user constraints
+            pass
 
-            for param in search_params: 
-                if (param.size() == 0)
-                    continue
-                else if (param.size() == 1)
-                    i = 0
-                else
-                    i = randint(1, param.size() - 1)
-                
-                self.search_state[i].append( (param, i) );
+        # Choose best scoring neighbor
 
-
-    def search( self ):
-        # TODO: Determine how to establish initial target values to optimize
-        # TODO: Determine interface to start simulations, retrieve results
+        return sys_config
 
 
-        
+def main():
+    search = DSE_searcher(None)
+    search.search()
+
+if __name__ == "__main__":
+    main()
