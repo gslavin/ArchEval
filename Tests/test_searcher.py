bc79db6a828fd7a15a7c8f396a0091b8d5ef6834
grs52 <grs52@ECE-BSN615-L01.coe.drexel.edu>
2016-11-14 16:47:34 -0500

    Added more tests for DSE_searcher.
    Added a test the uses the mock sim wrapper.

--- Tests/test_searcher.py
+++ Tests/test_searcher.py
@@ -3,6 +3,27 @@
 import unittest
 
 from DSE_searcher import DSE_searcher
+from mock_sim import MockSim
+
+def eval_stats(stats):
+    """
+    Basic function to minimize
+    """
+    #TODO:  How to calibrate parameters?
+    a = 10e9
+    b = 1/(1048576)
+    result = a*stats["execution time (s)"] + b*stats["Area (mm2)"]
+
+    return result
+
+def mock_eval_sys_config(sys_config):
+    """
+    Runs the configuration through the mock simulation
+    """
+    mock_sim = MockSim(sys_config)
+
+    mock_sim.run()
+    return eval_stats(mock_sim.stats)
 
 class TestSearcher(unittest.TestCase):
     def test_defaults(self):
@@ -24,6 +45,8 @@ class TestSearcher(unittest.TestCase):
         s.search(lambda x : - x["cache_size"] - x["cpu_frequency"] - x["cpu_count"])
 
     def test_invalid_args(self):
+        s = DSE_searcher(None, {})
+
         with self.assertRaises(ValueError):
             s = DSE_searcher(None, [])
 
@@ -32,6 +55,13 @@ class TestSearcher(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             s = DSE_searcher(None, {}, max_iterations = -1)
+
+        with self.assertRaises(ValueError):
+            s = DSE_searcher(None, param_ranges = [1,2,3])
+
+    def test_mock_sim(self):
+        search = DSE_searcher(user_constraints = None, param_ranges = {})
+        search.search(mock_eval_sys_config)
 
 if __name__ == '__main__':
     unittest.main()
